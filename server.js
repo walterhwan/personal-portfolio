ac624a86f1987f6dbc488b5fae6bb2637ab33651
Walter Wan <WalterHWan@gmail.com>
2018-05-14 19:12:39 -0700

    Fix port problem

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  // console.log('Example app listening on port 3000!');
 });
