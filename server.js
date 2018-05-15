31600b4f1539299a739b1d9d35d5c0c15e36016d
Walter Wan <WalterHWan@gmail.com>
2018-05-14 19:01:17 -0700

    Add server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/assets'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html');
 });
 
 app.listen(3000, function () {
